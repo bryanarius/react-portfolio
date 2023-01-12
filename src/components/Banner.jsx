b8ef15016330b8e325304ac1ba3abdc68cdf478a
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-01-11 20:38:29 -0500

    Small Changes
    - Added react bootstrap icon dependency
    - Updated banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,7 @@
 import {Col, Row, Container } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import headerImg from "../assets/img/header-img.svg"
+
 export const Banner = () => {
     return (
         <section className="banner" id="home">
@@ -6,6 +9,12 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi I'm Bryan`}<span className="wrap">Web Developer</span></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra aliquet eget sit amet tellus cras. Scelerisque felis imperdiet proin fermentum.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
                     </Col>
                 </Row>
             </Container>
