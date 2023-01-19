15ac532a091c0f6b8d8cd9e7b212e0696546860e
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-01-19 18:27:59 -0500

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { Newsletter } from "./Newsletter.jsx"
+import logo from "../assets/img/logo.svg"
+
+
+export const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Newsletter />
+                    <Col sm={6}>
+                        <img src={logo} alt="Logo" />
+                    </Col>
+                    <Col sm={6} className="text-center text-sm-end">
+                        <div className="text-center text-sm-end">
+                            <a href=""><img src={logo}></img></a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -1,22 +1,20 @@
-import MailchimpSubscribe from "mailchimp-subscribe";
-import { Newsletter } from "./Newsletter"
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter";
 
 export const MailchimpForm = () => {
-    const postUrl = ``
 
-    return (
-        <>
-            <MailchimpSubscribe
-                url={postUrl}
-                render={({ subscribe, status, message}) =>(
-                    <Newsletter
-                        status={status}
-                        message={message}
-                        onValidated={formData => subscribe(formDate)}
-                    />
-                )}
+  return (
+    <>
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <Newsletter
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
             />
-        </>
-    )
+        )}
+        />
+    </>
+  )
 }
-
